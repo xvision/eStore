abfa9d28b7ec4a191cccc3a877473ad24c87a7d2
xvision <xvision@3013.goyal.me>
2013-11-11 16:49:47 +0530

    Rails app
    cd estore
    rails new estore -T -d postgresql
    cd estore
    rbenv local 2.0.0-p247
    bundle install
    echo '/vendor/bundle' >> .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Estore::Application.config.secret_key_base = 'af663b7fec8abfd00aa4b6a2a63544729fb4dc8a0f772f6fca4f76794417c0e75e9e20f2289e89328cd1afa2d6477f90a539327e3a64972c288e081766460678'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Estore::Application.config.session_store :cookie_store, key: '_estore_session'

4b6138d47c31ad291aead65e75810848fe949872
xvision <xvision@3013.goyal.me>
2013-11-26 20:27:33 +0530

    Pulled from John

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,9 +1,9 @@
 class WelcomeMailer < ActionMailer::Base
-  default from: 'hello@jhola.com'
+  default from: 'notifications@2013.goyal.me'
  
   def welcome_email(email_to)
     mail(to: email_to,
-         subject: 'Welcome to Jhola',
+         subject: 'Welcome to My Awesome Site',
          template_name: 'welcome_email').deliver
   end
 end
